fabf3e26a01fa51d8fb472fa5877f95d34d7d395
pouya tirdad <pouyatirdad70@gmail.com>
2023-07-06 23:22:56 +0330

    add auction hub

---
+++ SignalR_Auction/Hubs/AuctionHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Auction.Models;
+
+namespace SignalR_Auction.Hubs
+{
+    public class AuctionHub :Hub
+    {
+        public async Task NotifyNewBid(AuctionNotify auctionNotify)
+        {
+            await Clients.All.SendAsync("ReceiveNewBid",
+                auctionNotify);
+        }
+    }
+}
--- SignalR_Auction/Program.cs
+++ SignalR_Auction/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using SignalR_Auction.Hubs;
 using SignalR_Auction.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSignalR(o => o.EnableDetailedErrors = true);
+builder.Services.AddSignalR();
 builder.Services.AddSingleton<IAuctionRepo, AuctionMemoryRepo>();
 
 var app = builder.Build();
@@ -32,5 +34,7 @@ app.MapPost("auction/{auctionId}/newbid", (int auctionId, int currentBid, IAucti
 {
     auctionRepo.NewBid(auctionId, currentBid);
 });
+
+app.MapHub<AuctionHub>("/auctionhub");
 
 app.Run();
